d044b9bf93fae5d61c8fb2bcbd3202206d4cfe67
Hack Reactor Student <student@hackreactor.com>
2017-09-13 20:08:51 -0700

    Implement basic GET and POST functionality

--- server/request-handler.js
+++ server/request-handler.js
@@ -12,7 +12,7 @@ this file and include it in basic-server.js so that it actually works.
 
 **************************************************************/
 
-var requestHandler = function(request, response) {
+
   // Request and Response come from node's http module.
   //
   // They include information about both the incoming request, such as
@@ -27,24 +27,15 @@ var requestHandler = function(request, response) {
   // Adding more logging to your server can be an easy way to get passive
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
   // The outgoing status.
-  var statusCode = 200;
-
   // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
-
   // Tell the client we are sending them plain text.
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
-
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
   // response.end() will be the body of the response - i.e. what shows
@@ -52,9 +43,6 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
-};
-
 // These headers will allow Cross-Origin Resource Sharing (CORS).
 // This code allows this server to talk to websites that
 // are on different domains, for instance, your chat client.
@@ -64,10 +52,102 @@ var requestHandler = function(request, response) {
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
+  // storage elements are objects with the following keys:
+    // .roomname, .username, .text, .objectId
+
+
+
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
+  'access-control-max-age': 10, // Seconds.
+  'Content-Type': 'application/json'
 };
+
+var storage = {};
+
+// storage['/'] = {results: [initMsg]};
+
+
+
+var requestHandler = function(request, response) {
+
+  var statusCode = 200;
+
+  var headers = defaultCorsHeaders;
+
+  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+
+  if (request.method === 'GET') {
+    headers['Content-Type'] = 'text/json';
+    response.writeHead(statusCode, headers);
+    var item = JSON.stringify(storage[request.url]);
+    response.end(item);
+
+  } else if (request.method === 'POST') {
+    request.on('data', (chunk) => {
+      if (!storage[request.url]) {
+        var item = JSON.parse(chunk.toString());
+        storage[request.url] = {};
+        storage[request.url].results = [];
+        storage[request.url].results.push(item);
+      } else {
+        var item = JSON.parse(chunk.toString());
+        storage[request.url].results.push(item);
+      
+      }
+    });
+    // var obj = storage[request.url].results ? storage[request.url].results.push(body) : body;
+    response.writeHead(statusCode, headers);
+    response.end('got it!');
+  }
+
+
+};
+
+
+exports.requestHandler = requestHandler;
+
+
+
+
+/*
+I - request
+  - GET
+  - POST
+O - GET
+  -- return JSON object which is an array of message objects
+  - POST
+  -- take the message data from the request object and push it into our array
+C - none
+E - none
+
+var message = {
+  username: app.username,
+  text: app.$message.val(),
+  roomname: app.roomname || 'lobby'
+};
+
+
+
+// check the AJAX method (request.method)
+  // if request.method === 'GET'
+    -- send back our data
+     else if request.method === 'POST'
+    -- add data to storage
+obj = {first: 'Jon', last}
+{name: 'Jon', name: obj}
+
+*/
+
+
+
+
+
+
+
+
+
+
 
